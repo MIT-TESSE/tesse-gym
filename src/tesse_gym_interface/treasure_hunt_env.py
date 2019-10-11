8344197d2ffdfac078d27185fe73056d4b86e50d
Zachary Ravichandran <zachary.ravichandran@ll.mit.edu>
2019-10-11 17:32:56 -0400

    small time delay so messages don't get dropped

--- src/tesse_gym_interface/treasure_hunt_env.py
+++ src/tesse_gym_interface/treasure_hunt_env.py
@@ -143,9 +143,12 @@ class TreasureHuntEnv(TesseEnv):
         """ Make agent take the specified action. """
         if action == 0:
             # forward, a bit of a hack to accommodate thin colliders
-            self.env.send(Transform(0, 0.2, 0))  # forward
             self.env.send(Transform(0, 0.2, 0))
+            time.sleep(0.025)  # so messages don't get dropped
+            self.env.send(Transform(0, 0.2, 0))
+            time.sleep(0.025)
             self.env.send(Transform(0, 0.1, 0))
+            time.sleep(0.025)
         elif action == 1:
             self.env.send(Transform(0, 0, 8))  # turn right
         elif action == 2:
