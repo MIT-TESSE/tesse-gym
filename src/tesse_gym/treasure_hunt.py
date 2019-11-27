375aa1ded94518d986f6a56360640b16ea1fb91a
Zachary Ravichandran <zachary.ravichandran@ll.mit.edu>
2019-11-27 15:21:57 -0500

    refactor reward function

--- src/tesse_gym/treasure_hunt.py
+++ src/tesse_gym/treasure_hunt.py
@@ -49,7 +49,7 @@ class HuntMode(Enum):
 
 class TreasureHunt(TesseGym):
     TARGET_COLOR = (245, 231, 50)
-    # TARGET_COLOR = (10, 138, 80)
+    # TARGET_COLOR = (10, 138, 80)  # for tesse v5.1 and above
     CAMERA_FOV = 45
 
     def __init__(
@@ -191,59 +191,43 @@ class TreasureHunt(TesseGym):
         """
         targets = self.env.request(ObjectsRequest())
         agent_data = observation
+        # track if agent changes environment (e.g. collects reward) so it can reobserve
         env_changed = False
 
         # compute agent's distance from targets
         agent_position = self._get_agent_position(agent_data.metadata)
         target_ids, target_position = self._get_target_id_and_positions(targets.metadata)
 
-        # TODO this logic need to be refactored
         reward = -0.01  # small time penalty
 
         # Reset if agent falls out of env
+        # TODO: tmp check until aggressive random spawn jitter is fixed
         if agent_position[1] < 0:
             self.done = True
             return reward, False
 
-        if target_position.shape[0] > 0:
-            # only compare (x, z) coordinates
-            agent_position = agent_position[np.newaxis, (0, 2)]
-            target_position = target_position[:, (0, 2)]
-            dists = np.linalg.norm(target_position - agent_position, axis=-1)
+        # check for found targets
+        if target_position.shape[0] > 0 and action == 3:
+            found_targets = self.get_found_targets(agent_position, target_position, target_ids, agent_data)
 
-            # can we see the target?
-            seg = agent_data.images[1]
-            target_in_fov = np.all(seg == self.TARGET_COLOR, axis=-1)
+            if len(found_targets):
+                # if in `MULTIPLE` mode, remove found targets
+                if self.hunt_mode is HuntMode.MULTIPLE:
+                    self.n_found_targets += len(found_targets)
+                    reward += self.target_found_reward * len(found_targets)
+                    self.env.request(RemoveObjectsRequest(ids=found_targets))
+                    env_changed = True
 
-            # if the agent is within `success_dist` of target, can see it,
-            # and gives the `found` action, count as found
-            if dists.min() < self.success_dist and target_in_fov.any() and action == 3:
-                found_targets = target_ids[dists < self.success_dist]
-                found_target_positions = target_position[dists < self.success_dist]
-                agent_orientation = self._get_agent_rotation(agent_data.metadata)[-1]
-                angles_rel_agent = self.get_target_angle_rel_agent(agent_orientation,
-                                                                   found_target_positions,
-                                                                   agent_position)
-                found_targets = found_targets[np.where(angles_rel_agent < self.CAMERA_FOV)]
-
-                if len(found_targets):
-                    # if in `MULTIPLE` mode, remove found targets
-                    if self.hunt_mode.value == HuntMode.MULTIPLE.value:  # TODO: need to compare values?
-                        self.n_found_targets += len(found_targets)
-                        reward += self.target_found_reward * len(found_targets)
-                        self.env.request(RemoveObjectsRequest(ids=found_targets))
-                        env_changed = True
-
-                        # if all targets have been found, restart the  episode
-                        if self.n_found_targets == self.n_targets:
-                            self._success_action()
-                            self.done = True
-
-                    # if in `SINGLE` mode, reset the episode
-                    elif self.hunt_mode == HuntMode.SINGLE:
-                        self._success_action()  # signal task was successful
-                        reward += self.target_found_reward
+                    # if all targets have been found, restart the  episode
+                    if self.n_found_targets == self.n_targets:
+                        self._success_action()
                         self.done = True
+
+                # if in `SINGLE` mode, reset the episode
+                elif self.hunt_mode == HuntMode.SINGLE:
+                    self._success_action()  # signal task was successful
+                    reward += self.target_found_reward
+                    self.done = True
 
         self.steps += 1
         if self.steps > self.max_steps:
@@ -254,13 +238,60 @@ class TreasureHunt(TesseGym):
 
         return reward, env_changed
 
-    def get_target_angle_rel_agent(self, agent_orientation, found_target_positions, agent_position):
+    def get_found_targets(self, agent_position, target_position, target_ids, agent_data):
+        """ Get targets that are within `self.success_dist` of agent and in FOV.
+
+        Args:
+            agent_position (np.ndarray): Agent position in (x, y, z) as a shape (3,) array.
+            target_position (np.ndarray): Target positions in (x, y, z) as a shape (n, 3) array.
+            target_ids (np.ndarray): Target IDS corresponding to position
+            agent_data (DataResponse): Agent observation data.
+
+        Returns:
+            List[int]: IDs of found targets.
+        """
+        found_targets = []
+
+        # only compare (x, z) coordinates
+        agent_position = agent_position[np.newaxis, (0, 2)]
+        target_position = target_position[:, (0, 2)]
+        dists = np.linalg.norm(target_position - agent_position, axis=-1)
+
+        # can we see the target?
+        seg = agent_data.images[1]
+        target_in_fov = np.all(seg == self.TARGET_COLOR, axis=-1)
+
+        # if the agent is within `success_dist` of target, can see it,
+        # and gives the `found` action, count as found
+        if dists.min() < self.success_dist and target_in_fov.any():
+            targets_within_dist = target_ids[dists < self.success_dist]
+            found_target_positions = target_position[dists < self.success_dist]
+            agent_orientation = self._get_agent_rotation(agent_data.metadata)[-1]
+            angles_rel_agent = self.get_target_orientation(agent_orientation,
+                                                           found_target_positions,
+                                                           agent_position)
+            found_targets = targets_within_dist[np.where(angles_rel_agent < self.CAMERA_FOV)]
+
+        return found_targets
+
+    def get_target_orientation(self, agent_orientation, target_positions, agent_position):
+        """ Get orientation of targets relative to agents given the agent position, orientation,
+        and target positions.
+
+        Args:
+            agent_orientation (float): Orientation of agent (y rotation) in radians.
+            target_positions (np.ndarray): Array of target (x, z) positions of shape (n, 2)
+                where n is the number of targets.
+            agent_position (np.ndarray): Array of agent (x, z) position of shape (1, 2)
+
+        Returns:
+            np.ndarray: Array of target orientations relative to agent.
+        """
         heading = np.array([[np.sin(agent_orientation), np.cos(agent_orientation)]])
-        target_relative_to_agent = found_target_positions - agent_position
+        target_relative_to_agent = target_positions - agent_position
         target_orientation = np.arccos(np.dot(heading, target_relative_to_agent.T) /
             (np.linalg.norm(target_relative_to_agent, axis=-1) * np.linalg.norm(heading)))
         return np.rad2deg(target_orientation).reshape(-1)
-
 
     def _success_action(self):
         """ Simple indicator that the agent has achieved the goal. """
