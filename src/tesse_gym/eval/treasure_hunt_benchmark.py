ad63b7a0309692859e25ff05518702440f1436b8
Zachary Ravichandran <zachary.ravichandran@ll.mit.edu>
2020-02-10 12:23:54 -0500

    simplify evaluation summary; add typehints

--- src/tesse_gym/eval/treasure_hunt_benchmark.py
+++ src/tesse_gym/eval/treasure_hunt_benchmark.py
@@ -1,50 +1,33 @@
 """ Treasure hunt evaluation """
-from collections import namedtuple
+from typing import Dict, List, Optional
 
 from ..treasure_hunt import MultiModalandPose
-from ..utils import set_multiple_camera_params
 from tesse.msgs import *
 
 from .benchmark import Benchmark
+from .agent import Agent
 
 
 EVALUATION_METRICS = ["found_targets", "precision", "recall", "collisions", "steps"]
-SceneResults = namedtuple("SceneResults", EVALUATION_METRICS)
-
-
-def combine_scene_results(scene_results):
-    """ Combine benchmark results across several scenes
-
-    Args:
-        scene_results (dict[str, SceneResults]):
-    """
-    results = {}
-    for metric in EVALUATION_METRICS:
-        results[metric] = sum(
-            [getattr(scene_result, metric) for scene_result in scene_results.values()]
-        )
-        if metric in ["precision", "recall"]:
-            results[metric] /= len(scene_results)
-    return SceneResults(**results)
 
 
 class TreasureHuntBenchmark(Benchmark):
     def __init__(
         self,
-        scenes,
-        episode_length,
-        n_targets,
-        environment_file,
-        success_dist,
-        launch_tesse,
-        random_seeds=None,
+        scenes: List[int],
+        episode_length: List[int],
+        n_targets: List[int],
+        environment_file: str,
+        success_dist: int,
+        launch_tesse: bool,
+        random_seeds: Optional[List[bool]] = None,
     ):
         """ Configure evaluation.
 
         Args:
             scenes (List[int]): Scene IDs.
-            episode_length (int): Episode length.
-            n_targets (int): Number of targets.
+            episode_length (List[int]): Episode length.
+            n_targets (List[int]): Number of targets.
             environment_file (str): Path to TESSE build.
             success_dist (int): Maximum distance from target to be considered found.
             launch_tesse (bool): True to spawn TESSE. Otherwise assume existing instance.
@@ -59,21 +42,21 @@ class TreasureHuntBenchmark(Benchmark):
             environment_file=environment_file,
             scene_id=self.scenes[0],
             success_dist=success_dist,
-            n_targets=n_targets,
-            max_steps=episode_length,
+            n_targets=n_targets[0],
+            max_steps=max(episode_length),
             step_rate=self.STEP_RATE,
-            init_hook=set_multiple_camera_params,
             launch_tesse=launch_tesse,
         )
 
-    def evaluate(self, agent):
+    def evaluate(self, agent: Agent) -> Dict[str, Dict[str, float]]:
         """ Evaluate agent.
 
         Args:
-            agent (tesse_gym.eval.agent.Agent): Agent to be evaluated.
+            agent (Agent): Agent to be evaluated.
 
         Returns:
-            int: Agent's score.
+            Dict[str, Dict[str, float]]: Results for each scene
+                and an overall performance summary.
         """
         results = {}
         for episode in range(len(self.scenes)):
@@ -91,7 +74,7 @@ class TreasureHuntBenchmark(Benchmark):
             agent.reset()
             obs = self.env.reset()
 
-            for step in range(self.episode_length):
+            for step in range(self.episode_length[episode]):
                 action = agent.act(obs)
                 obs, reward, done, info = self.env.step(action)
                 n_found_targets += info["n_found_targets"]
@@ -105,10 +88,23 @@ class TreasureHuntBenchmark(Benchmark):
 
             precision = n_found_targets / n_predictions
             recall = n_found_targets / self.env.n_targets
-            results[episode] = SceneResults(
-                n_found_targets, precision, recall, n_collisions, step + 1
-            )
+            results[str(episode)] = {
+                "found_targets": n_found_targets,
+                "precision": precision,
+                "recall": recall,
+                "collisions": n_collisions,
+                "steps": step + 1,
+            }
 
         self.env.close()
-        results["total"] = combine_scene_results(results)
+
+        # combine scene results
+        results["total"] = {
+            metric: sum([scene_result[metric] for scene_result in results.values()])
+            for metric in EVALUATION_METRICS
+        }
+
+        for metric in ["precision", "recall"]:
+            results["total"][metric] /= len(self.scenes)
+
         return results
