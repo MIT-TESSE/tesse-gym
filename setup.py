4f5232fba0536168978a23532161a8f875152f23
Zac Ravichandran <zachary.ravichandran@ll.mit.edu>
2020-02-27 13:22:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tesse_gym",
-    version="0.1.2",
+    version="0.1.3+snapshot",
     description="TESSE OpenAI Gym python interface",
     packages=find_packages("src"),
     # tell setuptools that all packages will be under the 'src' directory
